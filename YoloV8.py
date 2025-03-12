e7ac202b2c154c3b2c75c27b72b9b04fd832be7b
edvar82 <fodose123@gmail.com>
2025-03-12 00:57:58 -0300

    Adding .py's

--- YoloV8.py
+++ YoloV8.py
@@ -3,7 +3,7 @@ from multiprocessing import freeze_support
 
 def main():
     model = YOLO("yolov8n.pt")  # Carrega o modelo pré-treinado
-    model.train(data="Dataset/Pascal_VOC/data.yaml", epochs=2)  # Treina o modelo
+    model.train(data="Dataset/Pascal_VOC/data.yaml", epochs=50)  # Treina o modelo
     
 
 if __name__ == '__main__':
